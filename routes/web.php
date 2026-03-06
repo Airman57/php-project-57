b238129c68ff0d2c45e3a69daf074e2f68f36caa
alexandr <airmanjke57@gmail.com>
2026-03-06 09:39:00 +0300

    task statuses created

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('task_statuses', TaskStatusController::class);
+
 // отправка тестого письма, потом удалить
 Route::get('/send-test-mail', function () {
     Mail::to('any@example.com')->send(new TestMail());
